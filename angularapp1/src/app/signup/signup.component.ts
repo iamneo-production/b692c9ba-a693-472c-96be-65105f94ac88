259abeadf32ddb442844d208c2b72b2041107307
My Computer <kranthikumar7255@gmail.com>
2022-02-23 22:27:17 +0530

    angularapp

---
+++ angularapp1/src/app/signup/signup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+  public red1="gray"
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
